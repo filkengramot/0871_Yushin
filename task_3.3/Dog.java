35694e63178590d5d7a64d04a139ca9e7a80abc4
filkengramot <69895928+filkengramot@users.noreply.github.com>
2020-09-10 00:04:42 +0300

    Create Dog.java

---
+++ task_3.3/Dog.java
@@ -0,0 +1,49 @@
+package Dog;
+
+/*Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создайте геттеры и сеттеры для всех переменных класса Dog.
+Требования:
+•	Программа не должна считывать данные с клавиатуры.
+•	У класса Dog должна быть переменная name с типом String.
+•	У класса Dog должна быть переменная age с типом int.
+•	У класса должен быть сеттер для переменной name.
+•	У класса должен быть геттер для переменной name.
+•	У класса должен быть сеттер для переменной age.
+•	У класса должен быть геттер для переменной age.
+*/
+
+
+public class Dog {
+    private String name;
+    private int age;
+
+    Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+    
+    public static void main(String[] args) {
+        Dog dog = new Dog ("Бобик", 5);
+        System.out.println(dog.age);
+        System.out.println(dog.name);
+        }
+     
+    }
+
+
