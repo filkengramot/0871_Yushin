73ae7f4a74d388ab01679a813cd731898cb9b984
filkengramot <69895928+filkengramot@users.noreply.github.com>
2020-09-14 17:01:15 +0300

    Create Main.java

---
+++ task_4.3/Main.java
@@ -0,0 +1,38 @@
+Создать абстрактный класс Animal с характеристиками животного.
+Создать класс Horse который наследуется от Animal, в классе Horse реализовать метод public void run(){ System.out.println("Игого, я поскакал(а)"); }
+Создать класс Pegasus который наследуется от Horse, в классе Pegasus реализовать метод public void fly(){ System.out.println("Игого, я полетел(а)"); }
+Создать объект лошади и вызвать метод run();
+Создать объект пегаса и вызвать метод fly();
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    abstract class Animal {
+        String speak;
+        boolean vegan;
+        boolean pony;
+        byte paws;
+
+    }
+
+    class Horse extends Animal {
+        public void run() {
+            System.out.println("Игого, я поскакал(а)");
+        }
+    }
+
+    class Pegasus extends Horse {
+        public void fly() {
+            System.out.println("Игого, я полетел(а)");
+        }
+    }
+
+    Horse horse = new Horse();
+    Pegasus pegasus = new Pegasus();
+        horse.run();
+        pegasus.fly();
+    }
+    
+}
