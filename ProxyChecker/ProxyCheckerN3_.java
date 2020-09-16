71ce159dfad141174822e3c08bf4d3205bb62191
filkengramot <69895928+filkengramot@users.noreply.github.com>
2020-09-16 08:42:14 +0300

    Rename ProxyCheckerN3.java to ProxyCheckerN3_.java

--- ProxyChecker/ProxyCheckerN3_.java
+++ ProxyChecker/ProxyCheckerN3_.java
@@ -3,7 +3,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
 
-public class Main {
+public class ProxyCheckerN3 {
     public static void main(String[] args) throws Exception {
         FileReader reader = new FileReader("C://java/test.txt");
         int c;
